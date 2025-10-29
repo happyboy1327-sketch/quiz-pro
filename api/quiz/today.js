73aab889cbdf8f412c9a49c123e488d4825a20ce
happyboy1327-sketch <happyboy1327@gmail.com>
2025-10-29 16:33:57 +0900

    Change image URLs to stable links in today.js
    Updated image URLs to stable direct links.

--- api/quiz/today.js
+++ api/quiz/today.js
@@ -6,20 +6,20 @@ export default function handler(req, res) {
   // ************* 🚨 모든 이미지 URL이 안정적인 직접 링크로 변경됨 🚨 *************
   const allCandidates = [
     { name: "이순신", hint: "임진왜란 장군", image: "img/General-soonsin.jpg" },
-    { name: "세종대왕", hint: "한글 창제", image: "https://upload.wikimedia.org/wikipedia/commons/d/d4/King_Sejong_the_Great.jpg" },
-    { name: "정약용", hint: "목민심서 저술", image: "https://upload.wikimedia.org/wikipedia/commons/f/ff/Jeong_Yak-yong.jpg" },
-    { name: "아인슈타인", hint: "상대성이론 창시자", image: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Albert_Einstein_Head.jpg" },
-    { name: "마리 퀴리", hint: "라듐 발견", image: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Marie_Curie_c1920.jpg" },
-    { name: "간디", hint: "인도 독립운동가", image: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Portrait_Gandhi.jpg" },
-    { name: "링컨", hint: "미국 남북전쟁 대통령", image: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Abraham_Lincoln_O-77_matte_collodion_print.jpg" },
-    { name: "피카소", hint: "입체파 화가", image: "https://upload.wikimedia.org/wikipedia/commons/7/74/Pablo_picasso_1.jpg" },
-    { name: "레오나르도 다 빈치", hint: "모나리자 화가", image: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Leonardo_self.jpg" },
-    { name: "나폴레옹", hint: "프랑스 황제", image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Jacques-Louis_David_-_Napoleon_in_his_Study_-_Google_Art_Project_2.jpg" },
-    { name: "신사임당", hint: "율곡 이이의 어머니", image: "https://upload.wikimedia.org/wikipedia/commons/f/ff/Shin_Saimdang.jpg" },
-    { name: "안중근", hint: "이토 히로부미 저격 의사", image: "https://upload.wikimedia.org/wikipedia/commons/6/62/Ahn_Jung-geun_portrait.jpg" },
-    { name: "소크라테스", hint: "철학의 아버지", image: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Socrates_Louvre.jpg" },
-    { name: "아리스토텔레스", hint: "플라톤의 제자", image: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Aristotle_Altemps_Inv8575.jpg" },
-    { name: "뉴턴", hint: "만유인력 법칙 발견", image: "https://upload.wikimedia.org/wikipedia/commons/3/39/Sir_Isaac_Newton_%281642-1727%29.jpg" }
+    { name: "세종대왕", hint: "한글 창제", image: "https://commons.wikimedia.org/wiki/Special:FilePath/King_Sejong_the_Great.jpg" },
+    { name: "정약용", hint: "목민심서 저술", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Jeong_Yak-yong.jpg" },
+    { name: "아인슈타인", hint: "상대성이론 창시자", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Albert_Einstein_Head.jpg" },
+    { name: "마리 퀴리", hint: "라듐 발견", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Marie_Curie_c1920.jpg" },
+    { name: "간디", hint: "인도 독립운동가", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Portrait_Gandhi.jpg" },
+    { name: "링컨", hint: "미국 남북전쟁 대통령", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Abraham_Lincoln_O-77_matte_collodion_print.jpg" },
+    { name: "피카소", hint: "입체파 화가", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Pablo_picasso_1.jpg" },
+    { name: "레오나르도 다 빈치", hint: "모나리자 화가", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Leonardo_self.jpg" },
+    { name: "나폴레옹", hint: "프랑스 황제", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Jacques-Louis_David_-_Napoleon_in_his_Study_-_Google_Art_Project_2.jpg" },
+    { name: "신사임당", hint: "율곡 이이의 어머니", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Shin_Saimdang.jpg" },
+    { name: "안중근", hint: "이토 히로부미 저격 의사", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Ahn_Jung-geun_portrait.jpg" },
+    { name: "소크라테스", hint: "철학의 아버지", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Socrates_Louvre.jpg" },
+    { name: "아리스토텔레스", hint: "플라톤의 제자", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Aristotle_Altemps_Inv8575.jpg" },
+    { name: "뉴턴", hint: "만유인력 법칙 발견", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Sir_Isaac_Newton_%281642-1727%29.jpg" }
   ];
   // ************* 🚨 수정된 부분 종료 🚨 *************
 
@@ -44,3 +44,4 @@ export default function handler(req, res) {
 
   res.status(200).json(selected);
 }
+
