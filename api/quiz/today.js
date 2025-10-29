d4b06257961f5a2197764c4a4bf5db123a9fd2a9
happyboy1327-sketch <happyboy1327@gmail.com>
2025-10-29 16:54:53 +0900

    Modify image URLs for all quiz candidates
    Updated image URLs to ensure stable direct links.

--- api/quiz/today.js
+++ api/quiz/today.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   // 전체 후보군
   // ************* 🚨 모든 이미지 URL이 안정적인 직접 링크로 변경됨 🚨 *************
   const allCandidates = [
-    { name: "이순신", hint: "임진왜란 장군", image: "img/General-soonsin.jpg" },
+    { name: "이순신", hint: "임진왜란 장군", image: "/img/General-soonsin.jpg" },
     { name: "세종대왕", hint: "한글 창제", image: "https://commons.wikimedia.org/wiki/Special:FilePath/King_Sejong_the_Great.jpg" },
     { name: "정약용", hint: "목민심서 저술", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Jeong_Yak-yong.jpg" },
     { name: "아인슈타인", hint: "상대성이론 창시자", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Albert_Einstein_Head.jpg" },
@@ -14,8 +14,8 @@ export default function handler(req, res) {
     { name: "링컨", hint: "미국 남북전쟁 대통령", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Abraham_Lincoln_O-77_matte_collodion_print.jpg" },
     { name: "피카소", hint: "입체파 화가", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Pablo_picasso_1.jpg" },
     { name: "레오나르도 다 빈치", hint: "모나리자 화가", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Leonardo_self.jpg" },
-    { name: "나폴레옹", hint: "프랑스 황제", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Jacques-Louis David - The Emperor Napoleon in His Study at the Tuileries - Google Art Project 2.jpg" },
-    { name: "신사임당", hint: "율곡 이이의 어머니", image: "img/Shin_Saimdang.jpg" },
+    { name: "나폴레옹", hint: "프랑스 황제", image: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project_2.jpg" },
+    { name: "신사임당", hint: "율곡 이이의 어머니", image: "/img/Shin_Saimdang.jpg" },
     { name: "안중근", hint: "이토 히로부미 저격 의사", image: "https://commons.wikimedia.org/wiki/Special:FilePath/An_Jung-geun.jpg" },
     { name: "소크라테스", hint: "철학의 아버지", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Socrates_Louvre.jpg" },
     { name: "아리스토텔레스", hint: "플라톤의 제자", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Aristotle_Altemps_Inv8575.jpg" },
@@ -44,6 +44,7 @@ export default function handler(req, res) {
 
   res.status(200).json(selected);
 }
+
 
 
 
