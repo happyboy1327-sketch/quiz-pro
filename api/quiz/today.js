bf25b353f2d6298bc7de227481a63ce7077b2f2e
happyboy1327-sketch <happyboy1327@gmail.com>
2025-10-30 09:20:16 +0900

    Ensure stable image URLs in quiz API
    Updated the API to ensure all image URLs are stable and directly accessible.

--- api/quiz/today.js
+++ api/quiz/today.js
@@ -16,7 +16,7 @@ export default function handler(req, res) {
     { name: "레오나르도 다 빈치", hint: "모나리자 화가", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Leonardo_self.jpg" },
     { name: "나폴레옹", hint: "프랑스 황제", image: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project_2.jpg" },
     { name: "신사임당", hint: "율곡 이이의 어머니", image: "/img/Shin_Saimdang.jpg" },
-    { name: "안중근", hint: "이토 히로부미 저격 의사", image: "https://commons.wikimedia.org/wiki/Special:FilePath/An_Jung-geun.jpg" },
+    { name: "안중근", hint: "이토 히로부미 저격 의사", image: "https://upload.wikimedia.org/wikipedia/commons/4/4e/An_Jung-geun.jpg" },
     { name: "소크라테스", hint: "철학의 아버지", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Socrates_Louvre.jpg" },
     { name: "아리스토텔레스", hint: "플라톤의 제자", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Aristotle_Altemps_Inv8575.jpg" },
     { name: "뉴턴", hint: "만유인력 법칙 발견", image: "https://commons.wikimedia.org/wiki/Special:FilePath/Portrait_of_Sir_Isaac_Newton,_1689_(brightened).jpg" }
@@ -44,6 +44,7 @@ export default function handler(req, res) {
 
   res.status(200).json(selected);
 }
+
 
 
 
